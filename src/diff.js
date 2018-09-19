7d435ffde345d96f1995c7fb3132a75008063f66
Edward Paget <edward@cassetteta.pe>
2018-09-19 12:02:33 -0400

    Fix for kong 0.14.1 where null keys aren't returned

--- src/diff.js
+++ src/diff.js
@@ -1,36 +1,40 @@
 const isValueSameOneArrayElement = (a, b) => {
-    return typeof a === 'string'
-        && Array.isArray(b)
-        && b.length === 1
-        && !isValueDifferent(a, b[0]);
+  return typeof a === 'string'
+    && Array.isArray(b)
+    && b.length === 1
+    && !isValueDifferent(a, b[0]);
 };
 
 const isValueDifferent = (a, b) => {
-    if (Array.isArray(a)) {
-        return !Array.isArray(b)
-            || a.length != b.length
-            || a.filter(x => b.indexOf(x) === -1).length > 0;
-    }
+  if (Array.isArray(a)) {
+    return !Array.isArray(b)
+      || a.length != b.length
+      || a.filter(x => b.indexOf(x) === -1).length > 0;
+  }
 
-    return JSON.stringify(a) !== JSON.stringify(b);
+  if (a === null || typeof a === 'undefined') {
+    return (b !== null && typeof b !== 'undefined');
+  }
+
+  return JSON.stringify(a) !== JSON.stringify(b);
 }
 
 export default (defined = {}, server = {}) => {
-    const keys = Object.keys(defined);
+  const keys = Object.keys(defined);
 
-    return keys.reduce((changed, key) => {
-        if (key === 'redirect_uri') {
-            // hack for >=0.8.2 that allows multiple redirect_uris,
-            // but accepts a string as well
-            if (isValueSameOneArrayElement(defined[key], server[key])) {
-                return changed;
-            }
-        }
-
-        if (isValueDifferent(defined[key], server[key])) {
-            return [...changed, key];
-        }
-
+  return keys.reduce((changed, key) => {
+    if (key === 'redirect_uri') {
+      // hack for >=0.8.2 that allows multiple redirect_uris,
+      // but accepts a string as well
+      if (isValueSameOneArrayElement(defined[key], server[key])) {
         return changed;
-    }, []);
+      }
+    }
+
+    if (isValueDifferent(defined[key], server[key])) {
+      return [...changed, key];
+    }
+
+    return changed;
+  }, []);
 };
