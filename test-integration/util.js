9c8f107a18ec839e18d7e191ebc285d422e10bf3
Kalil Smith-Nuevelle <kalil@adhocteam.us>
2019-07-09 10:19:00 -0500

    Clean up services and routes between tests

--- test-integration/util.js
+++ test-integration/util.js
@@ -88,6 +88,7 @@ const cleanupKong = async () => {
         plugins: results.plugins.map(plugin => ({ ...plugin, ensure: 'removed' })),
         upstreams: results.upstreams.map(upstream => ({ ...upstream, ensure: 'removed' })),
         certificates: results.certificates.map(certificate => ({ ...certificate, ensure: 'removed' })),
+        services: results.services.map(service => ({ ...service, ensure: 'removed' })),
     }, testAdminApi);
 };
 
