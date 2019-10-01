99f3e6a891ae11f283db7ec9e5bb0a962a5e9e27
Josh Salisbury <josh@adhocteam.us>
2019-10-01 13:42:04 -0500

    Update prune CLI description

--- src/cli.js
+++ src/cli.js
@@ -7,5 +7,5 @@ commander
     .allowUnknownOption()
     .command('apply', 'Apply config to a kong server', {isDefault: true})
     .command('dump', 'Dump the configuration from a kong server')
-    .command('prune', 'Remove any service, route, global plugin or service/route plugin marked "ensure: removed" from your config file')
+    .command('prune', 'Rewrites config omitting any service, route, global plugin or service/route plugin marked "ensure: removed"')
     .parse(process.argv);
