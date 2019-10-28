6433ed1965f4ace9d5144b0ba80c3ad5506d73da
Mike Lumetta <mike.lumetta@adhocteam.us>
2019-10-28 13:51:34 -0400

    Remove support for JS config files

--- src/configLoader.js
+++ src/configLoader.js
@@ -17,7 +17,8 @@ export function configLoader(configPath) {
     const rawConfig = fs.readFileSync(configPath, 'utf8');
     const compiledConfig = rawConfig.replace(/\$\{([A-Za-z_]+)\}\$/g, (match, variableName) => {
         if (process.env[variableName] === undefined) {
-            throw new Error(`Configuration value ${variableName} was not present in the environment`)
+            log.error(`Configuration value ${variableName} was not present in the environment`);
+            process.exit(1);
         }
         return process.env[variableName];
     });
@@ -28,28 +29,6 @@ export function configLoader(configPath) {
 
     if (/(\.json)/.test(configPath)) {
         return JSON.parse(compiledConfig);
-    }
-
-    if (/(\.js)/.test(configPath)) {
-        try {
-            let config = require(resolvePath(configPath));
-
-            if (config === null || typeof config !== 'object' || Object.keys(config).length == 0) {
-                log.error('Config file must export an object!\n' + CONFIG_SYNTAX_HELP);
-
-                return process.exit(1);
-            }
-
-            return config;
-        } catch (e) {
-            if (e.code === 'MODULE_NOT_FOUND' && e.message.indexOf(configPath) !== -1) {
-                log.error('File %s does not exist!', configPath);
-            } else {
-                log.error('Invalid config file!\n  ' + e.stack);
-            }
-
-            return process.exit(1);
-        }
     }
 }
 
