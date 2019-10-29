a0863501bc8b8dcf29e1176bbaa6f6130545860f
Mike Lumetta <mike.lumetta@adhocteam.us>
2019-10-29 15:41:29 -0400

    Write tests for configLoader

--- src/configLoader.js
+++ src/configLoader.js
@@ -3,15 +3,15 @@ import mapDeep from 'deepdash/mapDeep';
 import path from 'path';
 import yaml from 'js-yaml';
 
-const log = {
+export const log = {
     info: message => console.log(message.green),
     error: message => console.error(message.red)
 };
 
 export function configLoader(configPath) {
     if (!fs.existsSync(configPath)) {
-        log.error(`Supplied --path '${configPath}' doesn't exist`.red);
-        return process.exit(1);
+        log.error(`Supplied --path '${configPath}' doesn't exist`);
+        throw new Error(`Supplied --path '${configPath}' doesn't exist`);
     }
 
     let rawConfig;
@@ -21,7 +21,7 @@ export function configLoader(configPath) {
         rawConfig = JSON.parse(fs.readFileSync(configPath, 'utf8'));
     } else {
         log.error(`${configPath} is not a supported Kongfig config format. Supported formats are YAML and JSON.`);
-        process.exit(1);
+        throw new Error(`${configPath} is not a supported Kongfig config format`);
     }
 
     return mapDeep(rawConfig, configValue => {
@@ -29,16 +29,16 @@ export function configLoader(configPath) {
             return configValue;
         }
 
-        return configValue.replace(/\$\{(.+)\}/g, (match, variableName) => {
+        return configValue.replace(/\$\{(.+?)\}/g, (match, variableName) => {
             const allowedNameRegex = /^[_a-zA-Z0-9]+$/;
             if (!allowedNameRegex.test(variableName)) {
                 log.error(`Configuration variable name ${variableName} is invalid.\nAllowed characters are letters, numbers, and underscores.`);
-                process.exit(1);
+                throw new Error(`Configuration variable name ${variableName} is invalid`);
             }
             
             if (process.env[variableName] === undefined) {
                 log.error(`Configuration value ${variableName} was not present in the environment.`);
-                process.exit(1);
+                throw new Error(`Configuration value ${variableName} was not present in the environment.`);
             }
 
             return process.env[variableName];
---
+++ test/configLoader.test.js
@@ -0,0 +1,154 @@
+import expect from 'expect.js';
+import path from 'path';
+import yaml from 'js-yaml';
+import { configLoader, log } from '../src/configLoader';
+
+jest.mock('fs');
+import fs from 'fs';
+
+const realFs = jest.requireActual('fs');
+const rawYamlConfig = realFs.readFileSync(path.join(__dirname, 'files/configLoader.yml'), 'utf8');
+const rawJsonConfig = realFs.readFileSync(path.join(__dirname, 'files/configLoader.json'), 'utf8');
+
+describe('configLoader', () => {
+  beforeAll(() => {
+    fs.readFileSync.mockImplementation(filePath => {
+      if(/(\.yml)|(\.yaml)/.test(filePath)) {
+        return rawYamlConfig;
+      } else if (/(\.json)/.test(filePath)) {
+        return rawJsonConfig;
+      }
+      return '';
+    });
+
+    // define valid env variables in the config file
+    process.env.MY_BIG_SECRET = 'no one can know';
+    process.env.NOUN_1 = 'excess';
+    process.env.NOUN_2 = 'riches';
+
+    // suppress console error output: .red isn't defined, so these aren't useful
+    jest.spyOn(log, 'error').mockImplementation(() => {});
+  });
+
+  beforeEach(() => {
+    fs.existsSync.mockReturnValue(true);
+  });
+
+  describe('file errors', () => {
+    it('should throw if the file does not exist', () => {
+      fs.existsSync.mockReturnValue(false);
+      expect(() => {
+        configLoader('./myConfig.yml');
+      }).to.throwException("Supplied --path './myConfig.yml' doesn't exist");
+    });
+
+    it('should throw if the file is in an unsupported format', () => {
+      expect(() => {
+        configLoader('myConfig.js');
+      }).to.throwException("myConfig.js is not a supported Kongfig config format");
+    });
+  });
+
+  describe('supported formats', () => {
+    it('should load YAML config files', () => {
+      let config;
+      ['config.yml', 'config.yaml'].forEach(filename => {
+        expect(() => {
+          config = configLoader('config.yml');
+        }).not.to.throwException();
+
+        expect('my_yaml_config' in config).to.be.ok();
+        expect(config.my_yaml_config.property).to.be('value');
+      });
+    });
+
+    it('should load JSON config files', () => {
+      let config;
+      expect(() => {
+        config = configLoader('config.json');
+      }).not.to.throwException();
+      
+      expect('myJsonConfig' in config).to.be.ok();
+      expect(config.myJsonConfig.property).to.be('value');
+    });
+  });
+
+  describe('compiling config', () => {
+    it("should not modify values with no templated variables", () => {
+      const config = configLoader('config.yml');
+      expect(config.my_yaml_config.property).to.be('value');
+    });
+
+    it("shouldn't accidentally use other variable formats", () => {
+      const config = configLoader('config.yml');
+      [
+        "$$$_FAKE_VAR_$$$ is the old format",
+        "#{} isn't right either",
+      ].forEach(sampleValue => {
+        expect(config.my_yaml_config.bad_var_formats.includes(sampleValue)).to.be.ok();
+      });
+    });
+
+    it("shouldn't try to perform a string replace on non-string types", () => {
+      const config = configLoader('config.yml');
+      [
+        100,
+        12.8,
+        true,
+      ].forEach(sampleValue => {
+        expect(config.my_yaml_config.non_strings.includes(sampleValue)).to.be.ok();
+      });
+    });
+
+    describe('environment variable errors', () => {
+      let validConfig;
+      beforeAll(() => {
+        validConfig = yaml.safeLoad(rawYamlConfig);
+      });
+
+      afterEach(() => {
+        jest.spyOn(yaml, 'safeLoad').mockRestore();
+      });
+
+      it('should validate the environment variable name', () => {
+        const configWithError = {
+          ... validConfig,
+          invalid_name: 'you named this ${INVALID-NAME} - whoops!',
+        };
+        jest.spyOn(yaml, 'safeLoad').mockReturnValue(configWithError);
+
+        // define the env variable - that's not the problem here
+        process.env['INVALID-NAME'] = 'some value';
+        expect(() => configLoader('config.yml'))
+          .to.throwException("Configuration variable name INVALID-NAME is invalid");
+      });
+
+      it('should validate that the environment variable has a value', () => {
+        const configWithError = {
+          ... validConfig,
+          undefined_var: 'uh oh, ${FORGOT_TO_DEFINE_THIS}',
+        };
+        jest.spyOn(yaml, 'safeLoad').mockReturnValue(configWithError);
+
+        expect(() => configLoader('config.yml'))
+          .to
+          .throwException();
+      });
+    });
+
+    it('should replace valid defined environment variables', () => {
+      const config = configLoader('config.yml');
+      expect(config.my_yaml_config.my_secret_key).to.be('Key: no one can know');
+    });
+
+    it("should handle multiple values in one config value (avoid greedy matching errors)", () => {
+      let config;
+      expect(() => {
+        config = configLoader('config.yml');
+      }).not.to.throwException();
+
+      expect(config.my_yaml_config.dont_be_greedy)
+        .to.be('two env variables??? an excess of riches');
+    });
+  });
+});
