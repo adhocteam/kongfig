cd901e73a6ebbb41b4268689dcebd4a0efdd2641
Mike Lumetta <mike.lumetta@adhocteam.us>
2019-11-13 17:23:09 -0500

    Add new version executable (#32)
    * Add new version executable to supplement/replace kongfig --version
    
    * Update version to 3.1.0

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,10 @@
 import commander from 'commander';
 
 commander
-    .version(__VERSION__, '-v, --version')
+    .version(__VERSION__)
     .allowUnknownOption()
-    .command('apply', 'Apply config to a kong server', {isDefault: true})
+    .command('apply', 'Apply config to a kong server')
     .command('dump', 'Dump the configuration from a kong server')
     .command('prune', 'Rewrites config omitting any service, route, global plugin or service/route plugin marked "ensure: removed"')
+    .command('version', 'A temporary alias for kongfig --version to support locally installed kongfig < 3.0.1')
     .parse(process.argv);
